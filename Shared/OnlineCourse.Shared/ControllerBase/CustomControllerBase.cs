2a3a5a9cef28bb610b88f22e5dc563f03196ff4d
Yunus Emre Akıncı <emreakinci696@gmail.com>
2023-07-19 18:36:34 +0300

    Added CourseController

---
+++ Shared/OnlineCourse.Shared/ControllerBase/CustomControllerBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineCourse.Shared.Dtos;
+
+namespace OnlineCourse.Shared.ControllerBase
+{
+    public class CustomControllerBase: Microsoft.AspNetCore.Mvc.ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode,
+            };
+        }
+    }
+}
