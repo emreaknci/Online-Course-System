2b35a11525ad0ed0560ef0f88a752c9e4ef305f4
Yunus Emre Akıncı <emreakinci696@gmail.com>
2023-07-23 13:39:58 +0300

    Added IdentityServer

---
+++ IdentityServer/OnlineCourse.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using Duende.IdentityServer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using OnlineCourse.IdentityServer.Dtos;
+using OnlineCourse.IdentityServer.Models;
+using OnlineCourse.Shared.Dtos;
+
+namespace OnlineCourse.IdentityServer.Controllers
+{
+    [Authorize(IdentityServerConstants.LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto dto)
+        {
+            var user = new ApplicationUser()
+            {
+                UserName = dto.UserName,
+                Email = dto.Email,
+                City = dto.City,
+            };
+            var result = await _userManager.CreateAsync(user, dto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+            }
+
+            return NoContent();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
+            if (userIdClaim == null) return BadRequest();
+
+            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+            if (user == null) return BadRequest();
+
+            return Ok(new { Id = user.Id, UserName = user.UserName, Email = user.Email, City = user.City });
+
+        }
+    }
+}
--- Services/Catalog/OnlineCourse.Services.Catalog/Program.cs
+++ Services/Catalog/OnlineCourse.Services.Catalog/Program.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
 using OnlineCourse.Services.Catalog;
 using OnlineCourse.Services.Catalog.Settings;
@@ -7,11 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCatalogServices();
+builder.Services.AddCatalogServices(builder.Configuration);
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
 builder.Services.AddSingleton<IDatabaseSettings>(p => p.GetRequiredService<IOptions<DatabaseSettings>>().Value);
 var app = builder.Build();
@@ -23,6 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
